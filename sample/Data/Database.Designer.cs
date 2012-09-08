95513d3a889d0da9b5cf6994e9ef1e00b49b274e
Peijen Lin <peijen.lin@live.com>
2012-09-08 06:51:34 +0000

    Removed sample project
    Will be adding new sample projects for vs2010 and vs2012

--- sample/Data/Database.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Peijen Lin\Documents\GitHub\ef-code-second\sample\Data\Database.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
